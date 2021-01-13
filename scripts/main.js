0ebc026951969136e6ef39cf0f44239cf726a9a5
vinaykharayat <vinaykharayat@gmail.com>
2021-01-13 21:11:51 +0530

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// function dogYears(humanyears){
+//     var dogYears=0;
+//     var humanYears = document.querySelector("#humanYears").value;
+//     console.log("here" + humanYears);
+//     for(var i=0; i<humanYears; ++i){
+//         if(i<2){
+//             dogYears+=10.5;
+//         }else{
+//             dogYears+=4;
+//         }
+//     }
+//     console.log(dogYears);
+//     document.querySelector("#result").textContent = dogYears;
+// }
+// document.querySelector("#calculateAge").addEventListener("click", dogYears);
